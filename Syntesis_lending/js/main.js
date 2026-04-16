3e198760cdff4482e9d71902c5a401ab090c3660
Kedrorex <wingsavr@gmail.com>
2026-04-16 15:51:07 +0400

    1

--- Syntesis_lending/js/main.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('a[href^="#"]').forEach(a => {
-  a.addEventListener('click', e => {
-    e.preventDefault();
-    document.querySelector(a.getAttribute('href'))?.scrollIntoView({ behavior: 'smooth' });
-  });
-});
-// Добавить: валидацию формы, анимации при скролле (IntersectionObserver), аккордеон FAQ
